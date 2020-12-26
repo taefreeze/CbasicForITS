53380509486a7effd2d8b890d911094f3d44e4a9
TaeFreeze <potaekingdom@hotmail.com>
2020-12-26 20:36:43 +0700

    part 3

--- Program.cs
+++ Program.cs
@@ -6,8 +6,12 @@ namespace CBasic
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("I am student");
+            Console.WriteLine("\nWhat is your name? ");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
+            Console.Write("\nPress any key to exit");
+            Console.ReadKey(true);
         }
     }
 }
