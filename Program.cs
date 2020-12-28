8eed489a191c3bedcaca05ecd2bd040974d5d0e1
TaeFreeze <potaekingdom@hotmail.com>
2020-12-28 20:07:40 +0700

    part 4

--- Program.cs
+++ Program.cs
@@ -6,12 +6,22 @@ namespace CBasic
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\nWhat is your name? ");
-            var name = Console.ReadLine();
-            var date = DateTime.Now;
-            Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
-            Console.Write("\nPress any key to exit");
-            Console.ReadKey(true);
+            // Console.WriteLine("\nWhat is your name? ");
+            // var name = Console.ReadLine();
+            // var date = DateTime.Now;
+            // Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
+            // Console.Write("\nPress any key to exit");
+            // Console.ReadKey(true);
+            Volumn();
+        }
+        // ไม่คืนค่า และไม่รับพารมิเตอร์(input)
+        static void Volumn()
+        {
+            int a = 5;
+            int b = 5;
+            int c = 5;
+            int volumn = a * b * c;
+            Console.WriteLine("The volumn of 3D is : " + volumn);
         }
     }
 }
