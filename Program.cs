690156e24093f4ef0fff40aab01c355eafc9e662
TaeFreeze <potaekingdom@hotmail.com>
2020-12-29 14:47:20 +0700

    part 5

--- Program.cs
+++ Program.cs
@@ -12,7 +12,15 @@ namespace CBasic
             // Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
             // Console.Write("\nPress any key to exit");
             // Console.ReadKey(true);
-            Volumn();
+            // Volumn();
+            Console.WriteLine("Enter number a : ");
+            int a;
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number b : ");
+            int b;
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The result is  : " + Add(a,b));
         }
         // ไม่คืนค่า และไม่รับพารมิเตอร์(input)
         static void Volumn()
@@ -22,6 +30,11 @@ namespace CBasic
             int c = 5;
             int volumn = a * b * c;
             Console.WriteLine("The volumn of 3D is : " + volumn);
+        }
+        static int Add(int x,int y)
+        {
+            int result = x + y;
+            return result;
         }
     }
 }
