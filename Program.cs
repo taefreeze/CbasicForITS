cb89004adcb7fa221bea1ec87a369421c6576bb5
TaeFreeze <potaekingdom@hotmail.com>
2020-12-29 15:07:26 +0700

    Grade

--- Program.cs
+++ Program.cs
@@ -13,14 +13,15 @@ namespace CBasic
             // Console.Write("\nPress any key to exit");
             // Console.ReadKey(true);
             // Volumn();
-            Console.WriteLine("Enter number a : ");
-            int a;
-            a = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter number b : ");
-            int b;
-            b = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine("Enter number a : ");
+            // int a;
+            // a = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine("Enter number b : ");
+            // int b;
+            // b = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("The result is  : " + Add(a,b));
+            // Console.WriteLine("The result is  : " + Add(a,b));
+            Grade();
         }
         // ไม่คืนค่า และไม่รับพารมิเตอร์(input)
         static void Volumn()
@@ -35,6 +36,63 @@ namespace CBasic
         {
             int result = x + y;
             return result;
+        }
+        static void Grade()
+        {
+            // input score from user
+            int a,b,c,d,e;
+            Console.WriteLine("Enter number a : ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number b : ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter number c : ");
+            c = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter number d : ");
+            d = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter number e : ");
+            e = int.Parse(Console.ReadLine());
+
+            // result of score
+            int result = a + b + c + d + e;
+            if (result > 100)
+                {
+                    Console.WriteLine("Error");
+                }
+                else if (result >= 80)
+                {
+                    Console.WriteLine("Your grade is A");
+                }
+                else if (result >= 75)
+                {
+                    Console.WriteLine("Your grade is B+");
+                }
+                else if (result >= 70)
+                {
+                    Console.WriteLine("Your grade is B");
+                }
+                else if (result >= 65)
+                {
+                    Console.WriteLine("Your grade is C+");
+                }
+                else if (result >= 60)
+                {
+                    Console.WriteLine("Your grade is C");
+                }
+                else if (result >= 55)
+                {
+                    Console.WriteLine("Your grade is D+");
+                }
+                else if (result >= 50)
+                {
+                    Console.WriteLine("Your grade is D");
+                }
+                else 
+                {
+                    Console.WriteLine("Your grade is F");
+                }
         }
     }
 }
